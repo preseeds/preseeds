fef71b3f615f927a68a426085ed7ec1e4c7ea5f3
mien.pham <mien.pham@vexere.com>
2024-10-16 15:19:05 +0700

    fix token card

--- components/tokenCard.tsx
+++ components/tokenCard.tsx
@@ -13,57 +13,57 @@ interface TokenCardProps {
 }
 
 const TokenCard: React.FC<TokenCardProps> = ({
-  name,
-  symbol,
-  avatar,
-  isPoolCreated,
-  raisedAmount,
-  targetLiquidity,
-}) => {
+                                               name,
+                                               symbol,
+                                               avatar,
+                                               isPoolCreated,
+                                               raisedAmount,
+                                               targetLiquidity,
+                                             }) => {
   const totalRaised = parseFloat(formatEther(raisedAmount));
   const totalTarget = parseFloat(formatEther(targetLiquidity));
   const progressPercentage =
-    totalTarget > 0 ? (totalRaised / totalTarget) * 100 : 0;
+      totalTarget > 0 ? (totalRaised / totalTarget) * 100 : 0;
 
   return (
-    <div className="bg-gray-900 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow cursor-pointer">
-      <div className="flex items-center space-x-4 mb-4">
-        <div className="w-16 h-16 rounded-full overflow-hidden">
-          <Image
-            src={avatar}
-            width={64}
-            height={64}
-            alt={`${name} avatar`}
-            className="object-cover"
-          />
+      <div className="p-1 shadow-md hover:shadow-lg transition-shadow cursor-pointer border border-transparent hover:border-[#86EFAC]">
+        <div className="flex items-center space-x-2">
+          <div className="w-32 h-32 overflow-hidden">
+            <Image
+                src={avatar}
+                width={96}
+                height={96}
+                alt={`${name} avatar`}
+                className="object-cover"
+            />
+          </div>
+          <div>
+            <h3 className="text-xl font-bold text-white">{name}</h3>
+            <p className="text-sm text-gray-400">{symbol}</p>
+          </div>
         </div>
         <div>
-          <h3 className="text-xl font-bold text-white">{name}</h3>
-          <p className="text-sm text-gray-400">{symbol}</p>
+          {isPoolCreated ? (
+              <p>Pool has been created</p>
+          ) : (
+              <div>
+                <div className="font-semibold text-sm text-gray-300 mb-2">
+                  Funding Progress
+                </div>
+                <div className="w-full bg-gray-800 rounded-full h-4">
+                  <div
+                      className="bg-green-500 h-4 rounded-full transition-all"
+                      style={{ width: `${progressPercentage}%` }}
+                  />
+                </div>
+                <div className="text-sm text-gray-400 mt-2">
+                  {progressPercentage.toFixed(2)}% raised ({totalRaised} VIC out of{" "}
+                  {totalTarget} VIC)
+                </div>
+              </div>
+          )}
         </div>
       </div>
-      <div className="mt-4">
-        {isPoolCreated ? (
-          <p>Pool has beed created</p>
-        ) : (
-          <div>
-            <div className="font-semibold text-sm text-gray-300 mb-2">
-              Funding Progress
-            </div>
-            <div className="w-full bg-gray-800 rounded-full h-4">
-              <div
-                className="bg-green-500 h-4 rounded-full transition-all"
-                style={{ width: `${progressPercentage}%` }}
-              />
-            </div>
-            <div className="text-sm text-gray-400 mt-2">
-              {progressPercentage.toFixed(2)}% raised ({totalRaised} VIC out of{" "}
-              {totalTarget} VIC)
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
   );
 };
 
