881aa0250ac3dcd5b89e4e00c6cece3155a87520
preseeds-sunrise <mien.pham@vexere.com>
2024-10-16 13:58:12 +0700

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,44 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Providers from "./providers";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
+import ClientFavicon from "@app/ClientFavicon";
 
+// Load custom fonts
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+    src: "./fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+    src: "./fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
 });
 
+// Define page metadata
 export const metadata: Metadata = {
-  title: "Buy everything at PRESEED round",
-  description: "Buy everything at PRESEED round",
+    title: "Buy everything at PRESEED round",
+    description: "Buy everything at PRESEED round",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
-      >
+    return (
+        <html lang="en">
+        <head>
+            {/* Favicon is set in the head */}
+            <link id="favicon" rel="icon" href="/frame1.png" />
+        </head>
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
+        >
         <Providers>{children}</Providers>
-        <SpeedInsights />
-        <Analytics />
-      </body>
-    </html>
-  );
+        <ClientFavicon />
+        </body>
+        </html>
+    );
 }
