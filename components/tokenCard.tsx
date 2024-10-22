6ea4a2f9a296a66f297028a78866dc47c4263cc3
preseeds-sunrise <mien.pham@vexere.com>
2024-10-22 14:17:23 +0700

    call me sensei

--- components/tokenCard.tsx
+++ components/tokenCard.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { formatEther } from "viem";
 import { useState } from "react";
 
@@ -13,20 +12,32 @@ interface TokenCardProps {
     targetLiquidity: bigint;
 }
 
-const TokenCard: React.FC<TokenCardProps> = ({name, symbol, avatar, isPoolCreated, raisedAmount, targetLiquidity,}) => {
+const TokenCard: React.FC<TokenCardProps> = ({
+                                                 name,
+                                                 symbol,
+                                                 avatar,
+                                                 isPoolCreated,
+                                                 raisedAmount,
+                                                 targetLiquidity,
+                                             }) => {
     const [hoverColor, setHoverColor] = useState<string>("transparent");
+    const [isError, setIsError] = useState(false);
 
     const totalRaised = parseFloat(formatEther(raisedAmount));
     const totalTarget = parseFloat(formatEther(targetLiquidity));
-    const progressPercentage =
-        totalTarget > 0 ? (totalRaised / totalTarget) * 100 : 0;
+    const progressPercentage = totalTarget > 0 ? (totalRaised / totalTarget) * 100 : 0;
 
     const generateRandomColor = () => {
         const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
         setHoverColor(randomColor);
     };
+
     const resetBorderColor = () => {
         setHoverColor("transparent");
+    };
+
+    const handleError = () => {
+        setIsError(true);
     };
 
     return (
@@ -38,13 +49,27 @@ const TokenCard: React.FC<TokenCardProps> = ({name, symbol, avatar, isPoolCreate
         >
             <div className="flex items-center space-x-2">
                 <div className="w-32 h-32 overflow-hidden">
-                    <Image
-                        src={avatar}
-                        width={96}
-                        height={96}
-                        alt={`${name} avatar`}
-                        className="object-cover"
-                    />
+                    {!isError ? (
+                        <img
+                            src={avatar}
+                            width={96}
+                            height={96}
+                            alt={`${name} avatar`}
+                            className="object-cover"
+                            onError={handleError}
+                        />
+                    ) : (
+                        <video
+                            width={128}
+                            height={128}
+                            className="object-cover"
+                            muted
+                            loop
+                            autoPlay
+                        >
+                            <source src={avatar} type="video/mp4" />
+                        </video>
+                    )}
                 </div>
                 <div>
                     <h3 className="text-xl font-bold text-white">{name}</h3>
@@ -66,8 +91,7 @@ const TokenCard: React.FC<TokenCardProps> = ({name, symbol, avatar, isPoolCreate
                             />
                         </div>
                         <div className="text-sm text-gray-400 mt-2">
-                            {progressPercentage.toFixed(2)}% raised ({totalRaised} VIC out of{" "}
-                            {totalTarget} VIC)
+                            {progressPercentage.toFixed(2)}% raised ({totalRaised} VIC out of {totalTarget} VIC)
                         </div>
                     </div>
                 )}
